76da0294669e42d7b8efdc4afbf35f996c5a7129
Alexey Petin <alex_beholder@mail.ru>
2023-03-07 14:27:50 +0300

    Update

---
+++ src/test/java/ru.netology/MedicalServiceTests.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+public class MedicalServiceTests {
+    @Test
+    public void checkBloodPressureTest(){
+
+    }
+
+    @Test
+    public void checkTemperatureTest(){
+
+    }
+
+    @Test
+    public void medicalServiceTestsWithMockito(){
+
+    }
+}
