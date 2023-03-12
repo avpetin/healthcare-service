5e2c29e5cdb54beabec475c95df89c2ef776609b
Alexey Petin <alex_beholder@mail.ru>
2023-03-12 23:56:54 +0300

    Update1

--- src/test/java/ru.netology/MedicalServiceTests.java
+++ src/test/java/ru.netology/MedicalServiceTests.java
@@ -1,33 +1,77 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
+import ru.netology.patient.entity.HealthInfo;
+import ru.netology.patient.entity.PatientInfo;
+import ru.netology.patient.repository.PatientInfoFileRepository;
 import ru.netology.patient.repository.PatientInfoRepository;
 import ru.netology.patient.service.alert.SendAlertService;
 import ru.netology.patient.service.medical.MedicalService;
 import ru.netology.patient.service.medical.MedicalServiceImpl;
 
-import static org.mockito.Mockito.*;
+import java.math.BigDecimal;
 
 public class MedicalServiceTests {
-    PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
-    SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
     @Test
     public void checkBloodPressureTest(){
-        BloodPressure bloodPressure = Mockito.mock(BloodPressure.class);
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        HealthInfo healthInfo = Mockito.mock(HealthInfo.class);
+        Mockito.when(healthInfo.getBloodPressure())
+                .thenReturn(new BloodPressure(120, 80));
+        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
+        PatientInfo patientInfo = new PatientInfo(null, null, null, null, healthInfo);
+        Mockito.when(patientInfoRepository.getById(Mockito.anyString()))
+                .thenReturn(patientInfo);
+
         MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
-        doAnswer()
-        Mockito.verify(medicalService.checkBloodPressure(Mockito.anyString(), bloodPressure), Mockito.times(1));
+
+        medicalService.checkBloodPressure(Mockito.anyString(), new BloodPressure(120, 80));
+
+        Mockito.verify(sendAlertService,
+                Mockito.times(1));
     }
 
     @Test
     public void checkTemperatureTest(){
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        HealthInfo healthInfo = Mockito.mock(HealthInfo.class);
+        Mockito.when(healthInfo.getNormalTemperature())
+                .thenReturn(new BigDecimal(36.6));
+        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
+        PatientInfo patientInfo = new PatientInfo(null, null, null, null, healthInfo);
+        Mockito.when(patientInfoRepository.getById(Mockito.anyString()))
+                .thenReturn(patientInfo);
 
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
+        medicalService.checkTemperature(Mockito.anyString(), new BigDecimal(36.6));
+
+        Mockito.verify(sendAlertService,
+                Mockito.times(1));
     }
 
     @Test
-    public void medicalServiceTestsWithMockito(){
+    public void bloodPressureArgumentCaptureTest(){
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        HealthInfo healthInfo = Mockito.mock(HealthInfo.class);
+        Mockito.when(healthInfo.getBloodPressure())
+                .thenReturn(new BloodPressure(120, 80));
+        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
+        PatientInfo patientInfo = new PatientInfo(null, null, null, null, healthInfo);
+        Mockito.when(patientInfoRepository.getById(Mockito.anyString()))
+                .thenReturn(patientInfo);
 
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        medicalService.checkBloodPressure(Mockito.anyString(), new BloodPressure(120, 80));
+
+        Mockito.verify(Mockito.spy(medicalService.checkBloodPressure(Mockito.anyString(), new BloodPressure(120, 80))),
+                Mockito.times(1)).
+        Assertions.assertEquals(String.format("Warning, patient with id: %s, need help", patientInfo.getId()), argumentCaptor.getValue());
     }
 }
